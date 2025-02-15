9c80dab016ad3a514bb29ca4f7d4b4742cb39fe9
Sachin302cse <sachin.19cs302@citranchi.ac.in>
2025-02-15 16:33:08 +0530

    adding user entity

---
+++ src/main/java/com/example/Demo/_Project/user/User.java
@@ -0,0 +1,32 @@
+package com.example.Demo._Project.user;
+
+public class User {
+
+    private int id;
+    private String userName;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
