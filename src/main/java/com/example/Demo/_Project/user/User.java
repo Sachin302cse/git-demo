40741a62c0059a81bafde0dd36587b3d18c5895f
Sachin302cse <sachin.19cs302@citranchi.ac.in>
2025-02-15 16:35:17 +0530

    modifying user entity

--- src/main/java/com/example/Demo/_Project/user/User.java
+++ src/main/java/com/example/Demo/_Project/user/User.java
@@ -5,6 +5,15 @@ public class User {
     private int id;
     private String userName;
     private String password;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public int getId() {
         return id;
